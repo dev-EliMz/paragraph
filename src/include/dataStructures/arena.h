d08cc854087d2d3e61d0c53c017f75d6cd26f374
dev-EliMz <elielmunizs3@gmail.com>
2026-05-19 11:07:33 -0300

    Alinhamento de memoria ajeitado e arena struct size retirado por enquanto

--- src/include/dataStructures/arena.h
+++ src/include/dataStructures/arena.h
@@ -7,13 +7,13 @@ typedef struct arena Arena;
 #include <stdlib.h>
 
 int arena_new(void* buffptr, size_t buffSize, Arena **outptr);
-size_t arena_struct_size();
 
 size_t arena_getSize(Arena *arena);
 size_t arena_usedBytes(Arena *arena);
+size_t arena_remainingBytes(Arena *arena);
 
 void arena_reset(Arena *arena);
-void *arena_alloc(Arena *arena, size_t size);
+void *arena_alloc(Arena *arena, size_t size, size_t align);
 
 struct arena {
     uint8_t *memory;
@@ -21,15 +21,15 @@ struct arena {
     size_t offset;
 };
 
-size_t arena_struct_size() {
-    return sizeof(Arena);
-}
-
-size_t arena_getSize(Arena *arena) {
+size_t arena_getCapacity(Arena *arena) {
     return arena->capacity;
 }
 
 size_t arena_usedBytes(Arena *arena) {
+    return arena->offset;
+}
+
+size_t arena_remainingBytes(Arena *arena) {
     return arena->capacity - arena->offset;
 }
 
@@ -37,23 +37,44 @@ int arena_new(void *buffptr, size_t buffSize, Arena **outptr) {
     if (!buffptr) return 0;
     if (!outptr) return 0;
     
-    if (arena_struct_size() > buffSize) return 0;
+    if (sizeof(Arena) > buffSize) return 0;
 
     Arena *arena = (Arena*) buffptr;
     arena->offset = 0;
-    arena->memory = ((uint8_t*)buffptr) + arena_struct_size();
-    arena->capacity = buffSize - arena_struct_size();
+    arena->memory = ((uint8_t*)buffptr) + sizeof(Arena);
+    arena->capacity = buffSize - sizeof(Arena);
 
     *outptr = arena;
     return 1;
 }
 
-void *arena_alloc(Arena *arena, size_t size) {
-    if (!arena) return NULL;
-    if (size > arena->capacity - arena->offset) return NULL;
+size_t mem_align(size_t ptrOffset, size_t alignment) {
+    size_t modulo = ptrOffset & (alignment - 1);
 
-    void *ptr = arena->memory + arena->offset;
-    arena->offset += size;
+    if (modulo != 0) ptrOffset += alignment - modulo;
+
+    return ptrOffset;
+}
+
+/*
+ * devEli: align tem que ser potencia de 2, 
+ * geralmente a engine vai utilizar alocadores encapsulados mas se voce for utilizar o
+ * alocador de arena,
+ * passe o alinhamento manualmente ou o size_t do elemento/size_t do elemento mais 
+ * estrito da struct a ser alocada
+ * O ideal seria utilizar _Alignof(type) (C11)
+ * */
+
+void *arena_alloc(Arena *arena, size_t size, size_t align) {
+    if (!arena) return NULL;
+    if (!align) return NULL;
+    if ((align & (align - 1)) != 0) return NULL;
+
+    size_t alignedOffset = mem_align(arena->offset, align);
+    if (size > arena->capacity - alignedOffset) return NULL;
+
+    void *ptr = arena->memory + alignedOffset;
+    arena->offset = size + alignedOffset;
     return ptr;
 }
 
