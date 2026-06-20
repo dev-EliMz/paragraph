cd3a480d10723cbbf1e9a9cd38d601e528b7e1fa
elimzDesktop2 <elielmunizs3@gmail.com>
2026-06-20 16:27:26 -0300

    allocators inicial completo

--- src/include/dataStructures/allocators.h
+++ src/include/dataStructures/allocators.h
@@ -2,6 +2,7 @@
 #define DSA_ALLOCATORS_H
 
 #include "arena.h"
+#include <stdlib.h>
 
 typedef struct {
     void *context;
@@ -11,6 +12,12 @@ typedef struct {
             size_t size,
             size_t alignment
             );
+    
+    void *(*realloc)(
+            void *context,
+            void *ptr,
+            size_t size
+            );
 
     void (*free)(
             void *context,
@@ -18,7 +25,15 @@ typedef struct {
             );
 } DsaAllocator;
 
+typedef struct {
+    size_t elementSize;
+    size_t alignment;
+    float expFactor;
+} DsaDynamicConfig;
+
 void *heap_allocator(void *context, size_t size, size_t alignment) {
+    (void)alignment;
+    (void)context;
     return malloc(size);
 }
 
@@ -28,8 +43,23 @@ void *arena_allocator(void *context, size_t size, size_t alignment) {
 }
 
 void heap_free(void *context, void *ptr) {
+    (void)context;
     if (!ptr) return;
     free(ptr);
+}
+
+void *heap_reallocator(void *context, void *ptr, size_t size) {
+    (void)context;
+    return realloc(ptr, size);
+}
+
+DsaDynamicConfig dsa_dynamic_config_init(
+        size_t elementSize,
+        size_t alignment,
+        float expFactor
+        ) {
+    DsaDynamicConfig config = { elementSize, alignment, expFactor };
+    return config;
 }
 
 void *dsa_memmove();
